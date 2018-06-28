258de8c625935608987084949a4c0a746b3b9813
harryphelps <harryphelps1@gmail.com>
2018-06-28 06:41:50 +0000

    Add new function for updating stock of coins when change is issued

---
+++ vending_machine.1.py
@@ -0,0 +1,11 @@
+from byotest import *
+
+eur_coins = {100:20, 50:20, 20:20, 10:20, 5:20, 2:20, 1:20}
+
+def get_change():
+        
+    return {}
+
+test_are_equal(get_change(0), {})
+
+print("All tests pass!")
--- vending_machine.py
+++ vending_machine.py
@@ -1,16 +1,29 @@
 from byotest import *
 
-usd_coins = [100, 50, 25, 10, 5, 1]
-eur_coins = [100, 50, 20, 10, 5, 2, 1]
+usd_coins = {100:20, 50:20, 25:20, 10:20, 5:20, 1:20}
+eur_coins = {100:20, 50:20, 20:20, 10:20, 5:20, 2:20, 1:20}
 
 def get_change(amount, coins =eur_coins):
     change = []
-    for coin in coins:
+    for coin in sorted(coins, reverse=True):
         while coin <= amount:
             amount -= coin
             change.append(coin)
             
     return change
+
+def get_stock(coin_stock, change):
+    #for each coin in the change array, look up the key and decrease the stock by 1
+    if(coin_stock == {1:0}):   
+        return({1:0})   
+    
+    for denomination in coin_stock:
+        for coin in change:
+            if (coin == denomination):
+                coin_stock[coin] -= 1
+    
+    return coin_stock
+        
 
 test_are_equal(get_change(0),[])
 test_are_equal(get_change(1), [1])
@@ -24,5 +37,12 @@ test_are_equal(get_change(3), [2,1])
 test_are_equal(get_change(7), [5,2])
 test_are_equal(get_change(9), [5,2,2])
 test_are_equal(get_change(35, usd_coins), [25,10])
+
+test_are_equal(get_stock({1:0},[1]),{1:0})
+test_are_equal(get_stock({1:1},[1]),{1:0})
+test_are_equal(get_stock({1:2},[1]),{1:1})
+test_are_equal(get_stock({2:2},[2]),{2:1})
+test_are_equal(get_stock({2:5},[2,2]),{2:3})
+test_are_equal(get_stock({2:5,1:5},[2,1]),{2:4,1:4})
 
 print("All tests pass!")
